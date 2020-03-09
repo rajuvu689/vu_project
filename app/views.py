90031ee30a6589dc4d72d3294223e2ce0a10d24b
Rahajul Amin <rtsoftworldcontact@gmail.com>
2020-03-09 21:31:09 +0600

    Django tutorial folder 5 porjonto done successfully.

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render (request, 'index.html',{
-        'status':'working on the project'
-    })
+    context = {
+        'status':'working on the contact manager app',
+        'age':'22'
+    }
+    return render (request, 'index.html',context)
