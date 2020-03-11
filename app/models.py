c095a9968a33768ec0034f6d21c1591578abbc40
Rahajul Amin <rtsoftworldcontact@gmail.com>
2020-03-12 00:41:47 +0600

    Django tutorial folder 6 full complete

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-
+from django.utils.timezone import datetime
 # Create your models here.
 class Contact (models.Model):
     name = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
     phone = models.IntegerField()
     info = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50, choices=(
+    gender = models.CharField(max_length=50,choices=(
         ('male','Male'),
         ('female','Female')
     ))
     image = models.ImageField(upload_to='images/',blank=True)
-    date_added  = models.DateField(auto_now_add=True)
+    date_added  = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return self.name
