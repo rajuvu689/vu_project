93f6d0e008d1727dcd9ad79f48ece2e3a8acba8f
Rahajul Amin <rtsoftworldcontact@gmail.com>
2020-03-14 14:14:04 +0600

    django tutorial folder 8 done fully but few errors

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('detail/<int:id>/', views.detail, name="detail"),
+    # path('', views.home, name="home"),
+    path('', views.HomePageView.as_view(), name="home"),
+    # path('detail/<int:id>/', views.detail, name="detail"),
+    path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
-def home(request):
-    context = {
-        'contacts': Contact.objects.all()
-    }
-    return render (request, 'index.html',context)
+# def home(request):
+#     context = {
+#         'contacts': Contact.objects.all()
+#     }
+#     return render (request, 'index.html',context)
 
-    def detail(request,id):
-        context = {
-            'contact': get_object_or_404(Contact, pk=id)
-        }
-        return render(request,'detail.html',context)
+    # def detail(request,id):
+    #     context = {
+    #         'contact': get_object_or_404(Contact, pk=id)
+    #     }
+    #     return render(request,'detail.html',context)
+
+
+class HomePageView(ListView):
+    template_name = 'index.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+class ContactDetailView(DetailView):
+    template_name = 'detail.html'
+    model = Contact
+    context_object_name = 'contact'
