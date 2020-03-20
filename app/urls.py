263bdf32dc373b0efac5ecdaa88bb89128b32ce7
Rahajul Amin <rtsoftworldcontact@gmail.com>
2020-03-20 23:14:59 +0600

    Django tutorial folder 9 done successfully

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name="home"),
     # path('detail/<int:id>/', views.detail, name="detail"),
     path('detail/<int:pk>/', views.ContactDetailView.as_view(), name="detail"),
+    path('search/', views.search, name="search"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import Contact
 from django.views.generic import ListView,DetailView
+from django.db.models import Q
 
 # Create your views here.
 # def home(request):
@@ -25,3 +26,20 @@ class ContactDetailView(DetailView):
     template_name = 'detail.html'
     model = Contact
     context_object_name = 'contact'
+
+def search(request):
+    if request.GET:
+        search_term = request.GET['search_term']
+        search_results = Contact.objects.filter(
+        Q(name__icontains=search_term) |
+        Q(email__icontains=search_term) |
+        Q(info__icontains=search_term) |
+        Q(phone__icontains=search_term) 
+        )
+        context = {
+            'search_term': search_term,
+            'contacts': search_results
+        }
+        return render(request, 'search.html', context)
+    else:
+        return redirect('home')
